4ad0e8efa3a83a969142ed42e4dac8e78e02ec2b
ServerCrashed <aks3045@proton.me>
2025-03-24 11:11:03 +0530

    Implement operate function

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ const add = (a, b) => a+b;
 const sub = (a, b) => a-b;
 const multiply = (a, b) => a*b;
 const divide = (a, b) => a/b;
-let x=9, y=5;
-console.log(add(x,y));
-console.log(sub(x,y));
-console.log(multiply(x,y));
-console.log(divide(x,y));
+
+let x=0, y=0, op='';
+
+function operate(a, b, o){
+    switch(o){
+        case "+":
+            return add(a,b);
+            break;
+        case "-":
+            return sub(a,b);
+            break;
+        case "*":
+            return multiply(a,b);
+            break;
+        case "/":
+            return divide(a,b);
+            break;
+        default:
+            return "Invalid";
+    }
+}
