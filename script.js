2b87162d0de247a7367828b1b9ef131f4506e2c0
ServerCrashed <aks3045@proton.me>
2025-03-24 10:54:33 +0530

    Implement key operation functions

---
+++ script.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a+b;
+const sub = (a, b) => a-b;
+const multiply = (a, b) => a*b;
+const divide = (a, b) => a/b;
+let x=9, y=5;
+console.log(add(x,y));
+console.log(sub(x,y));
+console.log(multiply(x,y));
+console.log(divide(x,y));
