43ca8f1ef981bdb4b47678911c5242180ab14d20
ServerCrashed <aks3045@proton.me>
2025-03-25 10:51:49 +0530

    Make buttons functional
    - Limit results to two decimals

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sub = (a, b) => a-b;
 const multiply = (a, b) => a*b;
 const divide = (a, b) => a/b;
 
-let x=0, y=0, op='';
+let x, y, op='';
 
 function operate(a, b, o){
     switch(o){
@@ -29,4 +29,35 @@ const display = document.querySelector(".display");
 const clear = document.querySelector("#clear");
 clear.addEventListener("click", () => {
     display.textContent='';
+    x=0, y=0, op='';
 });
+
+const str = '=AC';
+const numButtons = document.querySelector(".buttons .nums");
+numButtons.addEventListener("click", (e) => {
+    if(!str.includes(e.target.textContent) && display.textContent.length<8){
+        let t;
+        t = e.target.textContent;
+        if(!x) x=+t;
+        else y=+t;
+        display.textContent+=t;
+    }
+})
+
+const opButtons = document.querySelector(".buttons .ops");
+opButtons.addEventListener("click", (e) => {
+    let t;
+    t = e.target.textContent;
+    op = t;
+    display.textContent+=t;
+})
+
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", () => {
+    if(x && y && op){
+        let res = operate(x,y,op);
+        res = +res.toFixed(2);
+        display.textContent = res;
+        x=res;
+    }
+})
